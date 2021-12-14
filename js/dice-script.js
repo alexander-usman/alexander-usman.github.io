e7692d010a6b08712faf01d98f839fc42beccd99
alexander-usman <87687140+alexander-usman@users.noreply.github.com>
2021-12-14 15:21:13 +1100

    Formatting

--- js/dice-script.js
+++ js/dice-script.js
@@ -1,24 +1,20 @@
 window.onload = function () {
   const btnRoll = document.querySelector(`.btnRoll`);
-  const divShowResult = document.querySelector(`.divShowResult`);
 
   const rollXDX = function () {
     let result = 0;
     const numRolls = document.querySelector(`.numRolls`).value;
     const numModifier = document.querySelector(`.numModifier`).value;
-    console.log(numRolls);
     const dWhat = document.querySelector(
       "input[name=diceValues]:checked"
     ).value;
-    console.log(dWhat);
 
     for (let i = 0; i < numRolls; i++) {
       result += Math.trunc(Math.random() * dWhat) + 1;
-      console.log(result);
     }
     result += numModifier;
 
-    divShowResult.textContent = `${result}`;
+    btnRoll.textContent = `Your Roll: ${result}`;
   };
 
   btnRoll.addEventListener(`click`, rollXDX);
