0817d8576c3e7d2ab48085c293af1a59fc087be1
alexander-usman <87687140+alexander-usman@users.noreply.github.com>
2021-12-14 14:02:16 +1100

    Create dicescript.js
    Initial commit

---
+++ js/dicescript.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+  // const frmRollDice = document.querySelector(`.diceForm`);
+  const btnRoll = document.querySelector(`.btnRoll`);
+
+  const rollXDX = function () {
+    let result = 0;
+    const numRolls = document.querySelector(".numRolls").value;
+    console.log(numRolls);
+    const dWhat = document.querySelector(
+      "input[name=diceValues]:checked"
+    ).value;
+    console.log(dWhat);
+
+    for (let i = 0; i < numRolls; i++) {
+      result += Math.trunc(Math.random() * dWhat) + 1;
+      console.log(result);
+    }
+    btnRoll.value = `Your Roll: ${result}`;
+  };
+
+  btnRoll.addEventListener(`click`, rollXDX);
+};
