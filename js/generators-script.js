e55e32ee6c3194bc70343a82632d46664750ad21
alexander-usman <87687140+alexander-usman@users.noreply.github.com>
2021-12-23 16:50:29 +1100

    Debugging getArt.

--- js/generators-script.js
+++ js/generators-script.js
@@ -466,10 +466,16 @@ const getGems = function (numRolls, dWhat, value) {
 
 const getArt = function (numRolls, dWhat, value) {
   let result = ``;
+  let totalRolls = 0;
   for (let i = 0; i < numRolls; i++) {
-    const roll = Math.trunc(Math.random() * dWhat) + 1;
+    totalRolls += Math.trunc(Math.random() * dWhat) + 1;
+  }
+
+  for (let j = 0; j < totalRolls; j++) {
+    const roll = Math.trunc(Math.random() * artworks.get(value).length);
     result += artworks.get(value)[roll] + ` `;
   }
+
   return result;
 };
 
