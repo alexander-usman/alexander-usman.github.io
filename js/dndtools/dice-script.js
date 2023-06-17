761f448e9369694ec36502acdcb70127f0620d63
alexander-usman <87687140+alexander-usman@users.noreply.github.com>
2023-06-17 13:11:00 +1000

    Update dice-script.js with TTS test.

--- js/dndtools/dice-script.js
+++ js/dndtools/dice-script.js
@@ -17,6 +17,27 @@ const rollXDX = function () {
   }
   result += numModifier;
   resultsDiv.innerHTML = `Your Roll: ${result} (${rollList})`;
+  
+  // TTS
+  // Setup
+  let voices = speechSynthesis.getVoices();
+  if(!voices.length){
+    let utterance = new SpeechSynthesisUtterance("");
+    speechSynthesis.speak(utterance);
+    voices = speechSynthesis.getVoices();
+  }
+  
+  // Speak
+  let speakData = new SpeechSynthesisUtterance();
+  speakData.volume = 1; // From 0 to 1
+  speakData.rate = 1; // From 0.1 to 10
+  speakData.pitch = 2; // From 0 to 2
+  speakData.text = result;
+  speakData.lang = 'en';
+  speakData.voice = voices[0];
+
+  speechSynthesis.speak(speakData);
+  
 };
 
 btnRoll.addEventListener(`click`, rollXDX);
