5bcc65d9ed4629479cefec353cc8b1c1b41936b1
Alexander Usman <alexander.usman@gmail.com>
2023-08-16 16:11:49 +1000

    Added footer component.

---
+++ js/components/footer.js
@@ -0,0 +1,22 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <footer class="footer">
+        <ul>
+          <li><a href="mailto:alexander.usman@gmail.com">email</a></li>
+          <li>
+            <a href="https://github.com/alexander-usman"
+              >github.com/alexander-usman</a
+            >
+          </li>
+        </ul>
+      </footer>
+    `;
+  }
+}
+
+customElements.define("comp-footer", Footer);
